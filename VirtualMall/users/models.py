d4e030aac23b8bbe20bfc6f400ce51b8d847d9e6
Haripriya Ravindran <hpravindran@gmail.com>
2021-11-12 20:50:35 +0530

    Backend models

--- VirtualMall/users/models.py
+++ VirtualMall/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class UserTable(models.Model):
+    userid = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30, null=True, blank=True)
+    email = models.EmailField(max_length=50, null=True, blank=True)
+    password = models.CharField(max_length=20, null=True, blank=True)
