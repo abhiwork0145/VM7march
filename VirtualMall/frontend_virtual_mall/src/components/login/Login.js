61e6963b61c4cbdfd848bee0784ceedf8200a559
Haripriya Ravindran <hpravindran@gmail.com>
2021-11-14 23:14:10 +0530

    Initial

--- VirtualMall/frontend_virtual_mall/src/components/login/Login.js
+++ VirtualMall/frontend_virtual_mall/src/components/login/Login.js
@@ -1,20 +1,28 @@
-import React  from 'react'
-import './Login.css'
-
+import React from 'react'
+import './Login.css';
+import Google from './images/google.jpeg';
+import Facebook from './images/facebook.png';
 
 function Login() {
-    
     return (
-      
-        <div>
-       
-         <button type="Button" class="Google"  >Google signin</button><br/><br/>
-        
-         <button type="Button" class="Facebook">Facebook signin</button><br/><br/>
-        
-          <h3 class="lo">Already have an account ? <a href = "#">Login </a></h3>
-         </div>
+        <>
+          <div className="login-container">
+              <p className="mall-heading" id="v-mall-heading">VMall</p>
+              <p className="mall-heading">Mall Owner Login</p> 
+              <div className="login-content">
+                <input className="login-input" type="email" required placeholder="Email ID"></input>
+                <input className="login-input" type="password" required placeholder="Password"></input>
+                <button className="login-buttons" id="normal-login-button" type="submit">Login</button>
+                <button className="login-buttons"><img src={Google} alt="" className="buttons-logo"/>Google Sign-In</button>
+                <button className="login-buttons"><img src={Facebook} alt="" className="buttons-logo"/>FaceBook Sign-In</button>
+              </div>   
+              <div className="more-container">
+                  <a href="#">Don't Have An Account? Register Here</a>
+                  <a href="#">Forgot Password? Reset Now</a>
+              </div>
+          </div>     
+        </>
     )
 }
 
-export default Login;
+export default Login
