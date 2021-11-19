263c92afa61525ba66cc2e4afa915cf0e7a59d75
MANESHARDAD <DK0236@digikull.com>
2021-11-20 00:55:24 +0530

    Login Page CSS Code

--- VirtualMall/frontend_virtual_mall/src/components/login/Login.js
+++ VirtualMall/frontend_virtual_mall/src/components/login/Login.js
@@ -3,54 +3,72 @@ import './Login.css';
 import { useState } from "react";
 import Google from './images/google.jpeg';
 import Facebook from './images/facebook.png';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons'
+import GoogleAuthLogin from "../login/GoogleAuthLogin"
 
-const Login = (props) =>{
+
+const Login = (props) => {
   const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+  const [password, setPassword] = useState("")
 
-    const emailChangeHandler = (event) => {
-        setEmail(event.target.value);
-    }
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  }
 
-    const passwordChangeHandler = (event) => {
-        setPassword(event.target.value);
-    }
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  }
 
-    const submitHandler = e =>{
-        e.preventDefault();
-        props.onLogin(email, password);
-    }
+  const submitHandler = e => {
+    e.preventDefault();
+    props.onLogin(email, password);
+  }
 
-    return (
-        <>
-          <div className="login-container">
-              <p className="mall-heading" id="v-mall-heading">VMall</p>
-              <p className="mall-heading">Mall Owner Login</p>
-              <form onSubmit={submitHandler} className="login-form"> 
-                <div className="login-content">
-                  <input className="login-input" type="email" placeholder="Email ID" 
+  return (
+    <>
+      <div className="login-container">
+        <div className="login_box">
+          <div className="title1"> Login Page </div>
+          <div className="title2"> ( Mall Owner Login ) </div>
+          <div className="field">
+            <form onSubmit={submitHandler} >
+              <div className="login-content">
+                <div>
+                  <FontAwesomeIcon className="fontawsome" icon={faUser} />
+                  <input className="login-input" type="email" placeholder="Email ID"
                     value={email}
                     onChange={emailChangeHandler}
                     required
                   />
+                </div>
+                <div>
+                  <FontAwesomeIcon className="fontawsome" icon={faLock} />
                   <input className="login-input" type="password" placeholder="Password"
                     value={password}
                     onChange={passwordChangeHandler}
                     required
                   />
-                  <button className="login-buttons" id="normal-login-button" type="submit">Login</button>
-                  <button className="login-buttons"><img src={Google} alt="" className="buttons-logo"/>Google Sign-In</button>
-                  <button className="login-buttons"><img src={Facebook} alt="" className="buttons-logo"/>FaceBook Sign-In</button>
-                </div> 
-              </form>  
-              <div className="more-container">
-                  <Link to= "/signup">Don't Have An Account? Register Here</Link>
-                  {/* <a href="#">Forgot Password? Reset Now</a> */}
+                </div>
+                <button className="login_button" type="submit">Login</button>
+
+                <div className="GoogleLoginButton" >
+                  <GoogleAuthLogin />
+                </div>
+                { /* <button className="login-buttons"><img src={Google} alt="" className="buttons-logo" />Google Sign-In</button> */}
+                <button className="login-buttons"><img src={Facebook} alt="" className="buttons-logo" />FaceBook Sign-In</button>
               </div>
-          </div>     
-        </>
-    )
+              <div className="more-container">
+                <Link className="Forgot_pass" to="/signup">Don't Have An Account? Register Here</Link>
+                {/* <a href="#">Forgot Password? Reset Now</a> */}
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Login
--- VirtualMall/frontend_virtual_mall/src/components/signup/Signup.js
+++ VirtualMall/frontend_virtual_mall/src/components/signup/Signup.js
@@ -1,7 +1,11 @@
-import {React, useState} from 'react'
+import { React, useState } from 'react'
 import './Signup.css'
 import Google from './images/google.jpeg';
 import Facebook from './images/facebook.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import GoogleAuthLogin from "../login/GoogleAuthLogin"
+
 
 function Signup({ onSignup }) {
 
@@ -10,36 +14,56 @@ function Signup({ onSignup }) {
   const [password, setPassword] = useState("")
 
   const userChangeHandler = (event) => {
-      setUser(event.target.value);
+    setUser(event.target.value);
   }
 
   const emailChangeHandler = (event) => {
-      setEmail(event.target.value);
+    setEmail(event.target.value);
   }
 
   const passwordChangeHandler = (event) => {
-      setPassword(event.target.value);
+    setPassword(event.target.value);
   }
 
-  const submitHandler = e =>{
-      e.preventDefault();
-      onSignup(user, email, password);
+  const submitHandler = e => {
+    e.preventDefault();
+    onSignup(user, email, password);
   }
 
   return (
-    <div class = "container">
-      <h1 >VMALL </h1>
-      <h2>Mall Owner Registration</h2>
-        <form onSubmit={submitHandler}>             
-          <input type = "text" class="Name" placeholder="Full Name" onChange={userChangeHandler} /> <br/>
-          <input type = "text"  class="Number" id="Mobile Number" placeholder="Email" onChange={emailChangeHandler} /><br/>
-          <input type = "password" class="Password" placeholder="Password" onChange={passwordChangeHandler} /><br/>
-          <input type="submit" class = "next" value="Signup" /><br />
-          <button className="login-buttons"><img src={Google} alt="" className="buttons-logo"/>Google Sign-In</button>
-          <button className="login-buttons"><img src={Facebook} alt="" className="buttons-logo"/>FaceBook Sign-In</button>
-        </form>             
-    </div>  
-  )       
+    <>
+      <div className="signup_page">
+        <div className="container">
+          <h2 className="signup_title1">Ragister</h2>
+          <h2 className="signup_title2"> ( Mall Owner Registration )</h2>
+
+          <form onSubmit={submitHandler}>
+
+            <div className="form_group">
+              <FontAwesomeIcon className="fontawsome" icon={faUser} />
+              <input type="text" className="input_data" id="name" placeholder="Your Name" onChange={userChangeHandler} />
+            </div>
+            <FontAwesomeIcon className="fontawsome" icon={faEnvelope} />
+            <input type="text" className="input_data" id="Mobile Number" placeholder="Email" onChange={emailChangeHandler} /><br />
+
+            <FontAwesomeIcon className="fontawsome" icon={faLock} />
+            <input type="password" className="input_data" placeholder="Password" onChange={passwordChangeHandler} /><br />
+
+            <div>
+              <input type="submit" name="signup" id="signup" className="submit_button" value="Resister" />
+            </div>
+            <div className="GoogleLoginButton" >
+              <GoogleAuthLogin />
+            </div>
+
+           {/* <button className="login-buttons"><img src={Google} alt="" className="buttons-logo" />Google Sign-In</button> */}
+            <button className="login-buttons"><img src={Facebook} alt="" className="buttons-logo" />FaceBook Sign-In</button>
+
+          </form>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Signup
